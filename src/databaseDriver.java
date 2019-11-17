f28b07a6fab6829b9279187f16ade790bfbf4224
Simon Arneberg <arnebesp2531@uwec.edu>
2019-11-16 23:42:12 -0600

    exiting the dream world

--- src/databaseDriver.java
+++ src/databaseDriver.java
@@ -1,8 +1,11 @@
+import java.io.*;
+
 
 
 public class databaseDriver {
     public static void main(String args[]) {
-        System.out.println("hello world \n I wanna die");
+        System.out.println("hello world \nI wanna die");
+
 
 
 
@@ -11,4 +14,35 @@ public class databaseDriver {
 
 
     }
+
+
+
+
+
+    /**
+     * readEntry - read input string from console (no GUI)
+     * Creation date: (10/7/2002 10:55:56 AM)
+     */
+    public static String readEntry(String prompt)
+    {
+        try
+        {
+            StringBuffer buffer = new StringBuffer();
+            System.out.print(prompt);
+            System.out.flush();
+            int c = System.in.read();
+            while (c != '\n' && c != -1)    // while not newline or EOF
+            {
+                buffer.append((char)c);
+                c = System.in.read();
+            }
+            return buffer.toString().trim();
+        }
+        catch (IOException e)
+        {
+            return "";
+        }
+    }     // --- end - readEntry method
+
+
 }
