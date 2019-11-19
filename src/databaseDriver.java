1942548ef3ae5e30aaf110e450430dd7ecfff015
strecksh <strecksh8883@uwec.edu>
2019-11-19 16:46:26 -0600

    Simpon push the baby oyut

--- src/databaseDriver.java
+++ src/databaseDriver.java
@@ -10,29 +10,6 @@ public class databaseDriver {
     private int returnValue;                // return value for all other commands
 
     // --- connect() - connect to the Oracle database
-    public Connection connect() throws SQLException {
-        // --- set the username and password
-        String user = "STRECKSH8883";
-        String pass = "2C43J5R9";
-
-        // --- 1) get the Class object for the driver
-        try {
-            Class.forName("oracle.jdbc.OracleDriver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Could not get class object for Driver");
-        }
-
-        // --- 2) connect to database
-        try {
-            conn = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@alfred.cs.uwec.edu:1521:csdev", user, pass);
-        } catch (SQLException sqle) {
-            System.err.println("Could not make connection to database");
-            throw new SQLException();
-        }
-        return conn;
-    }    // end - method connect
-
 
     public static void main(String args[]) {
 
@@ -122,6 +99,95 @@ public class databaseDriver {
             return "";
         }
     }     // --- end - readEntry method
+
+    public void commit () {
+        try {
+            conn.commit();
+        }
+        catch (SQLException sqle) {
+            System.err.println("DAO, commit() - error in commit");
+            System.err.println(sqle.getMessage());
+        }
+    }	// end - method commit()
+
+
+    public void rollback () {
+        try {
+            conn.rollback();
+        }
+        catch (SQLException sqle) {
+            System.err.println("DAO, rollback() - error in rollback");
+            System.err.println(sqle.getMessage());
+        }
+    }	// end - method rollback()
+
+    public void disconnect () {
+        // --- 5) disconnect from database
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+            if (rset != null) {
+                rset = null;
+            }
+        }
+        catch (SQLException sqle) {
+            System.err.println ("Error in closing database connection");
+            System.err.println(sqle.getMessage());
+        }
+        finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                }
+                catch (SQLException sqlerb) {
+                    conn = null;
+                }
+            }
+            if (rset != null) {
+                try {
+                    rset = null;
+                }
+                catch (Exception e) {
+                    rset = null;
+                }
+            }
+        }
+    }	// end - method disconnect
+
+
+    public void setAutoCommit (boolean flag) {
+        try {
+            conn.setAutoCommit(flag);
+        }
+        catch (SQLException sqle) {
+            System.err.println("DAO, setAutoCommit() - error in setting");
+            System.err.println(sqle.getMessage());
+        }
+    }	// end - method setAutoCommit()
+
+    public Connection connect() throws SQLException {
+        // --- set the username and password
+        String user = "STRECKSH8883";
+        String pass = "2C43J5R9";
+
+        // --- 1) get the Class object for the driver
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not get class object for Driver");
+        }
+
+        // --- 2) connect to database
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@alfred.cs.uwec.edu:1521:csdev", user, pass);
+        } catch (SQLException sqle) {
+            System.err.println("Could not make connection to database");
+            throw new SQLException();
+        }
+        return conn;
+    }    // end - method connect
 
 
 }
